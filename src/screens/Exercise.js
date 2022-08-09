a9002e624bca07656f05b5fe90e789d040489f2b
younsonrisa <72068013+younsonrisa@users.noreply.github.com>
2022-08-10 08:37:03 +0900

    Update Exercise.js

--- src/screens/Exercise.js
+++ src/screens/Exercise.js
@@ -4,36 +4,53 @@ import {buttonStyles, titleStyles, imageStyles, HorizonLine, dateStyles, circleS
 
 const ExerciseMain = ({navigation}) => {
     const today = new Date();
-
     return(
         <SafeAreaView style={{flex:1, alignItems:'center', justifyContent:'flex-start', marginTop: 10}}>
             
             <View style={{alignItems:'space-around', flexDirection: 'row'}}>
        
-            <View style={{alignItems: 'flex-start', marginRight: 15}}>
+            <View style={{alignItems: 'flex-start', marginRight: 100}}>
             <Text style={titleStyles.lightText}>오늘도 고양과 함께</Text>
             <Text style={titleStyles.boldText}>운동하기</Text>
             </View>
             
             <View style={{alignItems: 'flex-start', marginLeft: 15}}>
-            <Image source={require('../../assets/exercisecat.png')} style={imageStyles.character}/>
+            <Image source={require('../../assets/exercisecat.png')} style={imageStyles.exercisecharacter}/>
             </View>
             </View>
-                
-            <Text style={HorizonLine.line}></Text>
 
-            <Text style = {circleStyles.circle}>
-                <Image source={require('../../assets/winkcat.png')} style={imageStyles.character}/>
-                <Text style = {circleStyles.titleText}>이화냥님의</Text>
-                <Text style = {circleStyles.titleText}>걸음 수</Text>
-                <Text style = {circleStyles.numText}> 30,000</Text>
-                <Text style = {circleStyles.walkText}> 걸음</Text>
-                <Text style = {circleStyles.titleText}>걸으셨습니다.</Text>
-            </Text>
-                        
-            <TouchableOpacity style={buttonStyles.exerciseButton} onPress={()=>navigation.navigate('MyWalk')}>     
-            <Text style={buttonStyles.buttonText}>통계 보러가기</Text>
-            </TouchableOpacity>
+            <Text style={HorizonLine.line}></Text>
+            <View style={{alignItems:'flex-start', marginRight:200}}>
+                <Text style={dateStyles.dateText}>DATE: 2022-04-15</Text>
+            </View>
+            
+
+            <View style = {circleStyles.circle}>
+                <View style={{alignItems:'space-around', flexDirection: 'row',}}>
+                    <View style={{alignItems:'flex-start', marginTop:0, marginLeft:0}}>
+                        <Image source={require('../../assets/winkcat.png')} style={imageStyles.winkcharacter}/>
+                    </View>
+                    <View style = {{alignItems:'flex-start', marginRight:0, marginLeft:10}}>
+                        <Text style = {circleStyles.titleText}>이화냥님의</Text>
+                        <Text style = {circleStyles.titleText}>걸음 수</Text>
+                    </View>
+                </View>
+                <View style = {{alignItems:'space-around', flexDirection: 'row', marginTop:10}}>
+                    <Text style = {circleStyles.numText}> 30,000</Text>
+                    <Text style = {circleStyles.walkText}> 걸음</Text>
+                </View>
+                <View style = {{marginTop:10}}>
+                    <Text style = {circleStyles.titleText}>걸으셨습니다.</Text>
+                </View>
+                
+            </View>
+
+            <View style={{marginTop:60}}>
+                <TouchableOpacity style={buttonStyles.exerciseButton} onPress={()=>navigation.navigate('MyWalk')}>     
+                <Text style={buttonStyles.buttonText}>통계 보러가기</Text>
+                </TouchableOpacity>
+            </View>           
+            
               
         </SafeAreaView>
     );
