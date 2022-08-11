ccb6cdf46f003a22a639a550f31a02d79bedbc6e
younsonrisa <72068013+younsonrisa@users.noreply.github.com>
2022-08-12 01:41:22 +0900

    Update PointChange.js

--- src/components/PointChange.js
+++ src/components/PointChange.js
@@ -1,11 +1,12 @@
-export default function PointChange(walk) {
-    let point = 100
-    let newpoint = 0
-    let newwalk = 0
-    return (
-        newpoint = point + (walk-(walk%100))/100
-    )
-    return (
-        newwalk = walk%100
-    )
+import React, {useState} from "react";
+
+function PointChange() {
+    const [walk, setWalk, point, setPoint] = useState(0);
+
+    const changeB = () => {
+        setPoint(prevPoint => prevPoint+(prevWalk-(prevWalk%100))/100)
+        setWalk(prevWalk => prevWalk%100);
+    }
+
+    return(walk, point)
 }
