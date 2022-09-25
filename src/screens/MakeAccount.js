718b10ac51d809c664aa1df8bc87873746088c0d
mutjin <104980932+mutjin@users.noreply.github.com>
2022-09-25 09:48:18 +0900

    Update MakeAccount.js

--- src/screens/MakeAccount.js
+++ src/screens/MakeAccount.js
@@ -8,17 +8,17 @@ export default function MakeAccountScreen({ navigation }) {
   return (
     <SafeAreaView style={viewStyles.whole}>
       <View style={viewStyles.titlecontainer}>
-        <Text style={textStyles.title1}>Make Account</Text>
+        <Text style={textStyles.title1}>계정 생성하기</Text>
       </View>
       <View style={{paddingVertical:7}}></View>
 
       <View style={viewStyles.contentscontainer}>
-        <Text style={textStyles.title2}>Make your Account with email</Text>
+        <Text style={textStyles.title2}>이메일로 계정 생성하기</Text>
         <View style={viewStyles.contents}>
-        <TextInput style={inputStyles.MakeAccountInput} placeholder="put your name"/>
-        <TextInput style={inputStyles.MakeAccountInput} placeholder="put your email address"/>
-        <TextInput style={inputStyles.MakeAccountInput} placeholder="put your password"/>
-        <TextInput style={inputStyles.MakeAccountInput} placeholder="put your password again"/>
+        <TextInput style={inputStyles.MakeAccountInput} placeholder="이름 입력"/>
+        <TextInput style={inputStyles.MakeAccountInput} placeholder="이메일 주소 입력"/>
+        <TextInput style={inputStyles.MakeAccountInput} placeholder="비밀번호 입력"/>
+        <TextInput style={inputStyles.MakeAccountInput} placeholder="비밀번호 재입력"/>
         </View>
       </View>
 
@@ -29,27 +29,27 @@ export default function MakeAccountScreen({ navigation }) {
         <ScrollView style={viewStyles.contents}>
           <View style={viewStyles.checkall}>
             <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-            <Text style={textStyles.agree}> agree all</Text>
+            <Text style={textStyles.agree}> 전체동의</Text>
           </View>
 
           <View style={viewStyles.checkone}>
             <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-            <Text style={textStyles.agree}> agree provisions?</Text>
+            <Text style={textStyles.agree}> 1. 약관동의?</Text>
           </View>
 
           <View style={viewStyles.checkone}>
             <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-            <Text style={textStyles.agree}> agree sharing your personal informations?</Text>
+            <Text style={textStyles.agree}> 2. 정보공유동의?</Text>
           </View>
 
           <View style={viewStyles.checkone}>
             <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-            <Text style={textStyles.agree}> agree that you are more than age 14?</Text>
+            <Text style={textStyles.agree}> 3. 14세 이상임을 동의?</Text>
           </View>
 
           <View style={viewStyles.checkone}>
             <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-            <Text style={textStyles.agree}> agree to receive marketing alarm?</Text>
+            <Text style={textStyles.agree}> 4. 마케팅 알람 수신 동의?</Text>
           </View>
         </ScrollView>
       </View>
