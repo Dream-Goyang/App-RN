8a59b0dff89c1f2c75cc93aa916cd63243d25d9b
mutjin <104980932+mutjin@users.noreply.github.com>
2022-09-25 09:46:30 +0900

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -9,26 +9,26 @@ export default function LoginScreen({ navigation }) {
       <Image source={require('../../assets/Login_title.png')} style={imageStyles.title}/>
     
       <View>
-        <TextInput style={inputStyles.LoginInput} placeholder="put your ID"></TextInput>
-        <TextInput style={inputStyles.LoginInput} placeholder="put your PW"></TextInput>
+        <TextInput style={inputStyles.LoginInput} placeholder="아이디 입력"></TextInput>
+        <TextInput style={inputStyles.LoginInput} placeholder="비밀번호 입력"></TextInput>
       </View>
 
       <TouchableOpacity style={buttonStyles.longButton} onPress={()=>navigation.navigate('Main')}>
-        <Text style={buttontextStyles.longText}>Login</Text>
+        <Text style={buttontextStyles.longText}>로그인</Text>
       </TouchableOpacity>
 
       <View style={viewStyles.autologin}>
         <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-        <Text style={{fontSize:15}}>auto login</Text>
+        <Text style={{fontSize:15}}>자동 로그인</Text>
       </View>
 
       <View style={{flexDirection:"row",}}>
         <TouchableOpacity style={buttonStyles.smallButton}>
-          <Text style={buttontextStyles.smallText}>  find id  |</Text>
+          <Text style={buttontextStyles.smallText}>  아이디 찾기  |</Text>
         </TouchableOpacity>
                             
         <TouchableOpacity style={buttonStyles.smallButton}>
-            <Text style={buttontextStyles.smallText}>  find pw  |</Text>
+            <Text style={buttontextStyles.smallText}>  비밀번호 찾기  |</Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={buttonStyles.smallButton} onPress={()=>navigation.navigate('MakeAccount')}>
