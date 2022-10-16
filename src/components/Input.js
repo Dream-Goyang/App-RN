4282063c69490d7a8abbb1f341d8ba4041b96811
younsonrisa <72068013+younsonrisa@users.noreply.github.com>
2022-10-16 16:15:28 +0900

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -8,7 +8,43 @@ export const inputStyles=StyleSheet.create({
       alignSelf:'center',
       fontSize:18,
       width:width-80,
+      height:60,import react from 'react';
+import { StyleSheet,TextInput,Dimensions } from "react-native";
+const width=Dimensions.get('window').width;
+const height=Dimensions.get('window').height;
+
+export const inputStyles=StyleSheet.create({
+  LoginInput:{
+      alignSelf:'center',
+      fontSize:15,
+      width:width-80,
       height:60,
+      marginTop:10,
+      marginLeft:3,
+      paddingLeft:15,
+      paddingTop:2,
+      borderRadius:5,
+      backgroundColor:'#FFFFFF',
+      color:'#696969',
+      borderWidth:1,
+      borderColor:'#696969',
+  },
+  MakeAccountInput:{
+    alignSelf:'center',
+    fontSize:15,
+    width:width-80,
+    height:40,
+    marginTop:10,
+    marginLeft:3,
+    paddingLeft:15,
+    paddingTop:2,
+    borderRadius:5,
+    backgroundColor:'#FFFFFF',
+    color:'#696969',
+    borderWidth:1,
+    borderColor:'#696969',
+},
+});
       marginTop:10,
       marginLeft:3,
       paddingLeft:15,
