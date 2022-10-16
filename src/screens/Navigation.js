66bf5aa83adcf28c78488272abd92d10db256651
younsonrisa <72068013+younsonrisa@users.noreply.github.com>
2022-10-16 16:12:19 +0900

    Update Navigation.js

--- src/screens/Navigation.js
+++ src/screens/Navigation.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './screens/Login';
+import findid from './screens/findid';
 import MakeAccountScreen from './screens/MakeAccount';
 import MainScreen from './screens/Main';
 import SightScreen from "./Sight";
@@ -19,17 +20,18 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Main">
-        <Drawer.Screen name="Login" component={LoginScreen} />
-        <Drawer.Screen name="MakeAccount" component={MakeAccountScreen} />
-        <Drawer.Screen name="Main" component={MainScreen} />
-        <Drawer.Screen name="Sight" component={SightScreen} />
-        <Drawer.Screen name="SightScreenMain" component={SightScreenMain} />
-        <Drawer.Screen name="SightScreenSub" component={SightScreenSub} />
-        <Drawer.Screen name="Game" component={GameScreen} />
-        <Drawer.Screen name="Exercise" component={ExerciseScreen} />
-        <Drawer.Screen name="MyWalk" component={MyWalkScreen} />
-        <Drawer.Screen name="Rank" component={RankScreen} />
+      <Drawer.Navigator initialRouteName="걸어갈고양">
+        <Drawer.Screen name="로그인" component={LoginScreen} />
+        <Drawer.Screen name="회원가입" component={MakeAccountScreen} />
+        <Drawer.Screen name="아이디/비밀번호찾기" component={findid} />
+        <Drawer.Screen name="걸어갈고양" component={MainScreen} />
+        <Drawer.Screen name="고양시명소보러가기" component={SightScreen} />
+        <Drawer.Screen name="명소보러가기" component={SightScreenMain} />
+        <Drawer.Screen name="관광지소개" component={SightScreenSub} />
+        <Drawer.Screen name="고양이찾기" component={GameScreen} />
+        <Drawer.Screen name="운동하기" component={ExerciseScreen} />
+        <Drawer.Screen name="나의통계" component={MyWalkScreen} />
+        <Drawer.Screen name="랭킹" component={RankScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
