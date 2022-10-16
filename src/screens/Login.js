242443a1b756424abe937a41c667584612a46f97
younsonrisa <72068013+younsonrisa@users.noreply.github.com>
2022-10-16 16:10:13 +0900

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -13,26 +13,26 @@ export default function LoginScreen({ navigation }) {
         <TextInput style={inputStyles.LoginInput} placeholder="비밀번호 입력"></TextInput>
       </View>
 
-      <TouchableOpacity style={buttonStyles.longButton} onPress={()=>navigation.navigate('Main')}>
-        <Text style={buttontextStyles.longText}>로그인</Text>
-      </TouchableOpacity>
-
       <View style={viewStyles.autologin}>
         <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
         <Text style={{fontSize:15}}>자동 로그인</Text>
       </View>
 
+      <TouchableOpacity style={buttonStyles.longButton} onPress={()=>navigation.navigate('걸어갈고양')}>
+        <Text style={buttontextStyles.longText}>로그인</Text>
+      </TouchableOpacity>
+
       <View style={{flexDirection:"row",}}>
-        <TouchableOpacity style={buttonStyles.smallButton}>
+        <TouchableOpacity style={buttonStyles.smallButton} onPress={()=>navigation.navigate('아이디/비밀번호찾기')}>
           <Text style={buttontextStyles.smallText}>  아이디 찾기  |</Text>
         </TouchableOpacity>
                             
-        <TouchableOpacity style={buttonStyles.smallButton}>
+        <TouchableOpacity style={buttonStyles.smallButton} onPress={()=>navigation.navigate('아이디/비밀번호찾기')}>
             <Text style={buttontextStyles.smallText}>  비밀번호 찾기  |</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={buttonStyles.smallButton} onPress={()=>navigation.navigate('MakeAccount')}>
-            <Text style={buttontextStyles.smallText}>  make account  </Text>
+        <TouchableOpacity style={buttonStyles.smallButton} onPress={()=>navigation.navigate('회원가입')}>
+            <Text style={buttontextStyles.smallText}>  회원가입  </Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
