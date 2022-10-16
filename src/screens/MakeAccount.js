bff51969b892a7be7681fce1bdbec61705150ca5
younsonrisa <72068013+younsonrisa@users.noreply.github.com>
2022-10-16 16:11:07 +0900

    Update MakeAccount.js

--- src/screens/MakeAccount.js
+++ src/screens/MakeAccount.js
@@ -8,56 +8,58 @@ export default function MakeAccountScreen({ navigation }) {
   return (
     <SafeAreaView style={viewStyles.whole}>
       <View style={viewStyles.titlecontainer}>
-        <Text style={textStyles.title1}>계정 생성하기</Text>
-      </View>
-      <View style={{paddingVertical:7}}></View>
-
-      <View style={viewStyles.contentscontainer}>
-        <Text style={textStyles.title2}>이메일로 계정 생성하기</Text>
-        <View style={viewStyles.contents}>
-        <TextInput style={inputStyles.MakeAccountInput} placeholder="이름 입력"/>
-        <TextInput style={inputStyles.MakeAccountInput} placeholder="이메일 주소 입력"/>
-        <TextInput style={inputStyles.MakeAccountInput} placeholder="비밀번호 입력"/>
-        <TextInput style={inputStyles.MakeAccountInput} placeholder="비밀번호 재입력"/>
-        </View>
+        <Text style={textStyles.title1}>회원가입</Text>
       </View>
 
-      <View style={{paddingVertical:7}}></View>
+      <View style={viewStyles.contentscontainer}>
+        <Text style={textStyles.title2}>이메일 회원가입</Text>
+        <ScrollView style={viewStyles.contents}>
+          <TextInput style={inputStyles.MakeAccountInput} placeholder="성명을 입력해주세요"/>
+          <TextInput style={inputStyles.MakeAccountInput} placeholder="이메일 주소"/>
+          <TextInput style={inputStyles.MakeAccountInput} placeholder="아이디"/>
+          <TextInput style={inputStyles.MakeAccountInput} placeholder="비밀번호 (영문+숫자+특수문자 조합 8자리 이상)"/>
+          <TextInput style={inputStyles.MakeAccountInput} placeholder="비밀번호 확인"/>
+        </ScrollView>
+      </View>
+
+      <View style={{paddingVertical:3}}></View>
 
       <View style={viewStyles.contentscontainer}>
-        <Text style={textStyles.title2}>You should agree below</Text>
+        <Text style={textStyles.title2}>약관 동의</Text>
         <ScrollView style={viewStyles.contents}>
           <View style={viewStyles.checkall}>
             <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-            <Text style={textStyles.agree}> 전체동의</Text>
+            <Text style={textStyles.allagree}> 전체동의</Text>
+          </View>
+
+          <View style={viewStyles.agreeline}></View>
+
+          <View style={viewStyles.checkone}>
+            <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
+            <Text style={textStyles.agree}> 1. 이용약관 동의 (필수)</Text>
           </View>
 
           <View style={viewStyles.checkone}>
             <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-            <Text style={textStyles.agree}> 1. 약관동의?</Text>
+            <Text style={textStyles.agree}> 2. 개인정보 수집 및 이용동의 (필수)</Text>
           </View>
 
           <View style={viewStyles.checkone}>
             <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-            <Text style={textStyles.agree}> 2. 정보공유동의?</Text>
+            <Text style={textStyles.agree}> 3. 만 14세 이상 이용자 (필수)</Text>
           </View>
 
           <View style={viewStyles.checkone}>
             <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-            <Text style={textStyles.agree}> 3. 14세 이상임을 동의?</Text>
-          </View>
-
-          <View style={viewStyles.checkone}>
-            <Image source={require('../../assets/UncheckedBox.png')} style={imageStyles.box}/>
-            <Text style={textStyles.agree}> 4. 마케팅 알람 수신 동의?</Text>
+            <Text style={textStyles.agree}> 서비스 마케팅 알림 동의 (필수)</Text>
           </View>
         </ScrollView>
       </View>
 
 
       <View style={viewStyles.itemcontainer}>
-        <TouchableOpacity style={buttonStyles.longButton} onPress={()=>navigation.navigate('Main')}>
-          <Text style={buttontextStyles.longText}>계정 생성하기</Text>
+        <TouchableOpacity style={buttonStyles.longButton} onPress={()=>navigation.navigate('로그인')}>
+          <Text style={buttontextStyles.longText}>회원가입</Text>
         </TouchableOpacity>
       </View>
 
