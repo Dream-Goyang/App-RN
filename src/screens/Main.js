eaeef9e74489bac3d93c94c7771e3fd764f3f801
younsonrisa <72068013+younsonrisa@users.noreply.github.com>
2022-10-16 16:10:43 +0900

    Update Main.js

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -7,36 +7,35 @@ export default function MainScreen({ navigation }) {
   return (
     <SafeAreaView style={viewStyles.whole}>
           <View style={viewStyles.profile}>
-            <Text style={textStyles.title}> walk goyang</Text>
-            <View style={{flexDirection:"row",paddingVertical:10,alignItems:'center',}}>
+            <Text style={textStyles.title}> 걸어갈고양</Text>
+            <View style={{flexDirection:"row",paddingVertical:30,alignItems:'center',}}>
               <Image source={MainImages.profile} style={imageStyles.profile}/>
               <View style={{justifyContent:'flex-start'}}>
-                <Text style={{fontSize:25,color:'white',}}>welcom Ewhanyang! </Text>
-                <Text style={{fontSize:15,color:'white',}}>walkingcat@naver.com </Text>
+                <Text style={{fontSize:26, color:'white', fontWeight:'700', paddingTop:10, paddingLeft:15}}>이화냥님 안녕하세요! </Text>
+                <Text style={{fontSize:15, color:'white', paddingTop:10, paddingHorizontal:15}}>walkingcat@naver.com </Text>
               </View>
             </View>
           </View>
 
       <View style={viewStyles.todaywalk}>
-        <Image source={MainImages.completed} style={imageStyles.box}/>
-        <Text style={textStyles.halfText}>today you walked</Text>
-        <Text style={textStyles.halfText}>30,000</Text>
+        <Text style={{fontSize:20, color:'white', paddingRight:55}}>✓  오늘의 걸음수</Text>
+        <Text style={{fontSize:20, color:'white', paddingLeft:5}}>30,000 걸음</Text>
       </View>
       
       <View style={viewStyles.lets}>
         
         
         <View style={viewStyles.greyline}>
-          <Text style={{fontSize:20,alignSelf:'flex-start',color:'grey',paddingLeft:10,}}> lets use walk goyang </Text>
+          <Text style={{fontSize:20,alignSelf:'flex-start',color:'black',}}> 걸어갈고양 이용하기 </Text>
         </View>
 
-        <TouchableOpacity onPress={()=>navigation.navigate('MyWalk')}>     
+        <TouchableOpacity onPress={()=>navigation.navigate('운동하기')}>     
           <View style={{paddingTop:25}}>
             <Image source={MainImages.letswalk} style={imageStyles.title}/>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={()=>navigation.navigate('SightScreenMain')}>     
+        <TouchableOpacity onPress={()=>navigation.navigate('고양시명소보러가기')}>     
           <View style={{paddingTop:25}}>
             <Image source={MainImages.letstour} style={imageStyles.title}/>
           </View>
