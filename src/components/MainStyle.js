68b3e515728f2db21d4764ec2d53223114269a7f
younsonrisa <72068013+younsonrisa@users.noreply.github.com>
2022-10-16 16:16:08 +0900

    Update MainStyle.js

--- src/components/MainStyle.js
+++ src/components/MainStyle.js
@@ -12,10 +12,12 @@ export const viewStyles=StyleSheet.create({
     },
     todaywalk:{
         flexDirection:"row",
-        backgroundColor:'#006655',
+        backgroundColor:'#2d8969',
         width:width,
         justifyContent:'center',
         height:40,
+        paddingVertical:10,
+        paddingHorizontal:10,        
     },
     profile:{
         backgroundColor:'#06ae7a',
@@ -32,7 +34,7 @@ export const viewStyles=StyleSheet.create({
     greyline:{
         borderBottomColor: 'lightgrey',
         borderBottomWidth: 2,
-        width:width,
+        width:width-30,
         paddingVertical:10,
     }
 });
@@ -57,15 +59,26 @@ export const imageStyles=StyleSheet.create({
 
 export const textStyles=StyleSheet.create({
     title:{
-        fontSize:40,
+        fontSize:35,
         alignSelf:'center',
         justifyContent:'center',
+        color:'white',
+        fontWeight: 'bold',
+        paddingTop:30,
+        paddingBottom:10,        
+    },
+    subtitle:{
+        fontSize:26,
+        color:'white',
+        fontWeight:'bold'
+    },
+    emailtext:{
+        fontSize:15,
         color:'white',
     },
     halfText:{
         width:width*0.4,
         textAlign:'center',
-        alignSelf:'center',
         color:'white',
         fontSize:20,
     },
